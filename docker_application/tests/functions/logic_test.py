3101187755daf0cb2c516c3e957246507c7c0445
Filipe Assis Mourão <filipemouraodsgit@gmail.com>
2024-10-05 08:44:04 -0300

    Initializing classes and creating the requirements.txt file

---
+++ docker_application/tests/functions/logic_test.py
@@ -0,0 +1,6 @@
+import unittest
+from docker_application.functions.logic import SimpleAppLogic
+
+class SimpleAppLogicTest(unittest.TestCase):
+    def test_simple_test(self):
+        self.assertTrue(True)
