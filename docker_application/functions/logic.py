644e31fae068a4442dea59e603e38399de07b3fc
Filipe Assis Mourão <filipemouraodsgit@gmail.com>
2024-10-05 09:01:54 -0300

    Initializing the test file

--- docker_application/functions/logic.py
+++ docker_application/functions/logic.py
@@ -1,2 +1,9 @@
-import numpy
+import numpy as np
 class SimpleAppLogic:
+    @staticmethod
+    def clean_input_data_points():
+        return True
+
+    @staticmethod
+    def fit_input_data():
+        return True
--- docker_application/functions/views.py
+++ docker_application/functions/views.py
@@ -1,2 +1,5 @@
 import plotly.graph_objects as go
 class SimpleAppViewer:
+    @staticmethod
+    def plot_fit_graph():
+        return True
